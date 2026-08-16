cfe3e7aa19686fb46e8203b2a2ae33f5c1622a8a
Soumyodipto68 <soumyodiptopal77@gmail.com>
2026-08-16 20:27:07 +0530

    Add v1 route integration to server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import express, { Request, Response } from "express";
 import morgan from "morgan";
 import cors from "cors";
 import config from "./config";
+import v1 from "./routes/v1";
+
 export const createServer = () => {
   const app = express();
   app
@@ -14,6 +16,6 @@ export const createServer = () => {
   app.get("/health", (req: Request, res: Response) => {
     res.json({ ok: true, environment: config.env });
   });
-
+  app.use("/v1", v1);
   return app;
 };
